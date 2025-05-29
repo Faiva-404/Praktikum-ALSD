ec730c8ca5238a6d37b9ae11151ba51b3e322caa
Faiva-404 <saharapuspa4@gmail.com>
2025-05-29 23:27:00 +0700

    jbsht11

--- Jobsheet11/SingleLinkedList09.java
+++ Jobsheet11/SingleLinkedList09.java
@@ -76,4 +76,90 @@ public class SingleLinkedList09 {
             }
         }
     }
+
+    public void getData(int index) {
+        NodeMahasiswa09 temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        temp.data.tampilInformasi();
+    }
+
+    public int indexOf(String key) {
+        NodeMahasiswa09 temp = head;
+        int index = 0;
+        while (temp != null && !temp.data.nama.equalsIgnoreCase(key)) {
+            temp = temp.next;
+            index++;
+        }
+        if (temp != null) {
+            return -1;
+        } else {
+            return index;
+        }
+    }
+
+    public void removeFirst() {
+        if (!isEmpty()) {
+            System.out.println("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+
+    public void removeLast() {
+        if (!isEmpty()) {
+            System.out.println("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            NodeMahasiswa09 temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+        }
+    }
+
+    public void remove(String key) {
+        if (isEmpty()) {
+            System.out.println("Linked List masih kosong, tidak dapat dihapus!");
+        } else {
+            NodeMahasiswa09 temp = head;
+            while (temp != null) {
+                if ((temp.data.nama.equalsIgnoreCase(key)) && (temp == head)){
+                    this.removeFirst();
+                    break;
+                } else if (temp.data.nama.equalsIgnoreCase(key)) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp; // Update tail if we removed the last element
+                    }
+                    break;
+                }
+                temp = temp.next;
+                
+            }
+        }
+    }
+
+    public void removeAt(int index) {
+        if (index == 0) {
+            removeFirst();
+        } else {
+            NodeMahasiswa09 temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (temp.next == null) {
+                tail = temp;
+            }
+        }
+    }
 }
