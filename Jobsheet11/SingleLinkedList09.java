c64feabd80d4552b7a30eb2b9cc09758169d4875
Faiva-404 <saharapuspa4@gmail.com>
2025-05-29 20:20:35 +0700

    Jbsht11

---
+++ Jobsheet11/SingleLinkedList09.java
@@ -0,0 +1,84 @@
+package Jobsheet11;
+
+public class SingleLinkedList09 {
+    NodeMahasiswa09 head;
+    NodeMahasiswa09 tail;
+
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            System.out.println("Isi Linked List:");
+            NodeMahasiswa09 tmp = head;
+            while (tmp != null) {
+                System.out.printf("%-10s %-5s %.1f\n", tmp.data.nama, tmp.data.nim, tmp.data.ipk);
+                tmp = tmp.next;
+            }
+            System.out.println("");
+        } else {
+            System.out.println("Linked List kosong");
+        }
+    }  
+    
+    public void addFirst(Mahasiswa09 input) {
+        NodeMahasiswa09 ndInput = new NodeMahasiswa09(input, null);
+        if (isEmpty()) {
+            head = ndInput;
+            tail = ndInput;
+        } else {
+            ndInput.next = head;
+            head = ndInput;
+        }
+    }
+
+    public void addLast(Mahasiswa09 input) {
+        NodeMahasiswa09 ndInput = new NodeMahasiswa09(input, null);
+        if (isEmpty()) {
+            head = ndInput;
+            tail = ndInput;
+        } else {
+            tail.next = ndInput;
+            tail = ndInput;
+        }
+    }
+
+    public void insertAfter(Mahasiswa09 input, String key) {
+        NodeMahasiswa09 ndInput = new NodeMahasiswa09(input, null);
+        NodeMahasiswa09 temp = head;
+        do { 
+            if (temp.data.nama.equalsIgnoreCase(key)) {
+                ndInput.next = temp.next;
+                temp.next = ndInput;
+                if (ndInput.next == null) {
+                    tail = ndInput; 
+                }
+                break;
+                
+            }
+            temp = temp.next;
+        } while (temp != null);
+    }
+
+    public void insertAt(int index, Mahasiswa09 input) {
+        if (index < 0) {
+            System.out.println("Indeks salah");
+        } else if (index == 0) {
+            addFirst(input);
+        } else {
+            NodeMahasiswa09 temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                if (temp == null) {
+                    System.out.println("Indeks melebihi jumlah elemen");
+                    return;
+                }
+                temp = temp.next;
+            }
+            temp.next = new NodeMahasiswa09(input, temp.next);
+            if (temp.next.next == null) {
+                tail = temp.next; 
+            }
+        }
+    }
+}
